b22b48c7f9e610a83eccd9b453d9e84d74e929a7
NouraMq <nouralmqbali@gmail.com>
2025-10-19 23:39:18 +0400

    add file

--- OOP/Models/Book.cs
+++ OOP/Models/Book.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Assignment.OOP.Models
 {
     internal class Book
     {
+        public int IsbnNo { get; set; }
+        public string Title { get; set; }
+        public string Athour { get; set; }
+        public string Year { get; set; }
+        public Book(int IsbnNo, string Titlee, string Athour, string Year)
+        {
+            this.IsbnNo = IsbnNo;
+            this.Title = Title;
+            this.Athour = Athour;
+            this.Year = Year;
+        }
     }
 }
--- OOP/Models/Person.cs
+++ OOP/Models/Person.cs
@@ -8,9 +8,32 @@ namespace Assignment.OOP.Models
 {
     internal class Person
     {
-        private int id;
-        private string name;
-        private int age;
+        //private int id;
+        //private string name;
+        //private string age;
+        //        bool IsMarried;
+        public int id { get; set; }
+        public string name { get; set; }
+        public string age { get; set; }
+        public bool IsMarried { get; set; }
 
+        public Person(int id, string name, string age, bool isMarried)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.IsMarried = isMarried;
+        }
+        //public int GetId()
+        //{
+        //    return this.id;
+        //}
+        //public string GetName()
+        //{
+        //    return this.name;
+        //}
+        //public void setName(string name)
+        //{
+        //    this.name = name;
     }
 }
