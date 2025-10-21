189350059909e0ecf38a49ab0b84d0c7980250f1
NouraMq <nouralmqbali@gmail.com>
2025-10-21 08:25:05 +0400

    add file

--- obj/Debug/net8.0/Assignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af77fdae49b4825f436fb0fa6c750b370e040be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c965a5fcf3a6ae652d188dc13fd698ffc13ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
